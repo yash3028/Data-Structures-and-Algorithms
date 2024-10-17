d141c0acb8518c6619a5a6fb7909801e4fdbbc6f
yash3028 <shyamakurayashwanth@gmail.com>
2024-10-17 23:14:24 +0530

    Updated File

--- string_sorting/lexo_sort.py
+++ string_sorting/lexo_sort.py
@@ -1,8 +1,12 @@
+def bubble(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0,n-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
 def lexo_sort(arr):
-    arr.sort()
-    st=[]
-    for i in arr:
-        st.append(i)
-    return st
-ans=lexo_sort(["bab","aab","csa"])
+    return bubble(arr)
+ans=lexo_sort(["bab","aab","ada"])
 print(ans)
